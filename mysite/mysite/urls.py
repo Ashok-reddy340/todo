b22490314c3cc05981e8d09e1b7778817185f962
Ashok-reddy340 <ashoklakki94.com>
2021-06-01 20:55:15 +0530

    create view todo for addtask

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', views.add,name='add'),
+
 ]
