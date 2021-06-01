c6a6bdd5f11cf0f764988604581f13d49322735e
Ashok-reddy340 <ashoklakki94.com>
2021-06-01 20:54:04 +0530

    adding post request for acces adminstration

--- mysite/Myapp/views.py
+++ mysite/Myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+# Create your views here
+def add(request):
+    if request.method == 'POST':
+        name = request.POST.get('name','')
+        priority = request.POST.get('priority','')
+        task = Task(name = name,priority = priority)
+        task.save()
+    return render (request,'Myapp/add.html')
