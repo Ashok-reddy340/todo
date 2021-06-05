a7df24f63a62796df5a41987745e5e8d3c48f396
Ashok-reddy340 <ashoklakki94.com>
2021-06-05 23:31:05 +0530

    create update method for update values

--- mysite/Myapp/views.py
+++ mysite/Myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Task
+from .forms import TodoForm
 
 # Create your views here
 #   if request.method == 'POST':
@@ -34,3 +35,11 @@ def delete(request,taskid):
         task.delete()
         return redirect('/')
     return render(request,'Myapp/delete.html',{'task':task})
+
+def update(request,id):
+    task = Task.objects.get(id=id)
+    form = TodoForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'Myapp/edit.html',{'form':form,'task':task})    
